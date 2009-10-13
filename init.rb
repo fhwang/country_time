ed8bbc241c44f608508be845821d57b98ff66247
Francis Hwang <sera@fhwang.net>
2009-10-13 00:33:16 -0400

    starting to absorb CountryCodes plugin

--- init.rb
+++ init.rb
@@ -1,7 +1,1 @@
 require 'country_time'
-require(
-  File.join(
-    RAILS_ROOT, 
-    "/vendor/plugins/country_time/vendor/country_codes/lib/country_codes"
-  )
-)
--- lib/country_time.rb
+++ lib/country_time.rb
@@ -14,12 +14,14 @@ module ActionView
       def country_options_for_select(selected = nil, priority_countries = nil)
         country_options = ""
         value_type = :a3
-        CountryCodes.load_countries_from_yaml
+        CountryTime::CountryCodes.load_countries_from_yaml
         priority_countries ||= CountryTime.high_priority_countries
 
         if priority_countries
           priority_country_options = priority_countries.map { |c_value|
-            cc = CountryCodes.send("find_by_#{value_type}", c_value)
+            cc = CountryTime::CountryCodes.send(
+              "find_by_#{value_type}", c_value
+            )
             [cc[:name], cc[value_type]]
           }
           country_options += options_for_select(
@@ -28,7 +30,7 @@ module ActionView
           country_options += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
         end
         all_countries =
-            CountryCodes.instance_variable_get(:@countries)['name'].values
+            CountryTime::CountryCodes.instance_variable_get(:@countries)['name'].values
         sorted_countries = all_countries.sort_by { |country| country[:name] }
         all_country_options = sorted_countries.map { |country|
           [country[:name], country[value_type]]
@@ -64,4 +66,71 @@ end
 
 module CountryTime
   mattr_accessor :high_priority_countries
+  
+  module CountryCodes # :nodoc:
+    def self.method_missing(name, *args)
+      if match = /find_([^_]*)_by_([^_]*)/.match(name.to_s)
+        raise "1 argument expected, #{args.size} provided." unless args.size == 1
+        
+        required = match[1]
+        request  = match[2]
+        if valid_attributes.include?(request) && valid_attributes.include?(required)
+          @countries[request][args[0].to_s.downcase][required.to_sym] || nil rescue nil
+        else
+          raise "#{request} is not a valid attribute, valid attributes for find_*_by_* are: #{valid_attributes.join(', ')}."
+        end
+        
+      elsif match = /find_by_(.*)/.match(name.to_s)
+        raise "1 argument expected, #{args.size} provided." unless args.size == 1
+        
+        request = match[1]     
+        if valid_attributes.include?(request)  
+          @countries[request][args[0].to_s.downcase] || nil_countries_hash
+        else
+          raise "#{request} is not a valid attribute, valid attributes for find_by_* are: #{valid_attributes.join(', ')}."
+        end
+        
+      else
+        raise NoMethodError.new("Method '#{name}' not supported")
+      end
+    end
+    
+    def self.countries_for_select(*args)
+      # Ensure that each of the arguments is a valid attribute
+      args.each do |arg|
+        unless valid_attributes.include?(arg)
+          raise "#{arg} is not a valid attribute, valid attributes are: #{valid_attributes.join(', ')}"
+        end
+      end
+      
+      # Build and return the desired array
+      @countries[@countries.keys.first].map do |index, country|
+        args.map { |a| country[a.to_sym] }
+      end
+    end
+    
+    def self.valid_attributes
+      @countries.keys
+    end
+    
+    def self.nil_countries_hash
+      hash = {}
+      valid_attributes.map { |attribute| hash[attribute.to_sym] = nil }
+      hash
+    end
+    
+    def self.load_countries_from_yaml
+      # Load countries
+      countries_from_file = YAML::load(
+        File.open("#{File.dirname(__FILE__)}/countries.yml")
+      )
+      
+      # Build indexes for each attribute
+      @countries = {}    
+      countries_from_file.first.keys.each do |key|
+        @countries[key.to_s] = {}
+        countries_from_file.each { |country| @countries[key.to_s][country[key].to_s.downcase] = country }
+      end
+    end
+  end
 end
