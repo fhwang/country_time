ce9742c98f5a955c2d34844a9bbbe7a39fb20782
Francis Hwang <sera@fhwang.net>
2009-10-14 22:33:17 -0400

    cleanup

--- lib/country_time.rb
+++ lib/country_time.rb
@@ -14,20 +14,16 @@ module ActionView
       def country_options_for_select(selected = nil, priority_countries = nil)
         country_options = ""
         value_type = :a3
-        priority_countries ||= CountryTime.high_priority_countries
-
-        if priority_countries
-          priority_country_options = priority_countries.map { |c_value|
-            country = CountryTime::Country.send(
-              "find_by_#{value_type}", c_value
-            )
-            [country.name, country.send(value_type)]
-          }
+        priority_country_options = CountryTime.priority_options_for_select(
+          priority_countries, value_type
+        )
+        if priority_country_options
           country_options += options_for_select(
             priority_country_options, selected
           )
           country_options += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
         end
+          
         all_countries = CountryTime::Country.all
         sorted_countries = all_countries.sort_by { |country| country.name }
         all_country_options = sorted_countries.map { |country|
@@ -65,47 +61,52 @@ end
 module CountryTime
   mattr_accessor :high_priority_countries
   
+  def self.priority_options_for_select(priority_countries, value_type)
+    priority_countries ||= CountryTime.high_priority_countries
+    if priority_countries
+      priority_countries.map { |c_value|
+        country = Country.send("find_by_#{value_type}", c_value)
+        [country.name, country.send(value_type)]
+      }
+    end
+  end
+  
   class Country
     @@countries_hashes = nil
     
-    def self.method_missing(name, *args)
-      if match = /find_([^_]*)_by_([^_]*)/.match(name.to_s)
-        raise "1 argument expected, #{args.size} provided." unless args.size == 1
-        
-        required = match[1]
-        request  = match[2]
-        if valid_attributes.include?(request) && valid_attributes.include?(required)
-          countries_hashes[request][args[0].to_s.downcase][required.to_sym] || nil rescue nil
-        else
-          raise "#{request} is not a valid attribute, valid attributes for find_*_by_* are: #{valid_attributes.join(', ')}."
-        end
-        
-      elsif match = /find_by_(.*)/.match(name.to_s)
-        raise "1 argument expected, #{args.size} provided." unless args.size == 1
-        
-        request = match[1]
-        if valid_attributes.include?(request)  
-          countries_hashes[request][args[0].to_s.upcase]
-        else
-          raise "#{request} is not a valid attribute, valid attributes for find_by_* are: #{valid_attributes.join(', ')}."
-        end
-        
-      else
-        super
-      end
-    end
-    
     def self.all
-      countries_hashes['name'].values
+      countries_hashes[:name].values
     end
     
     def self.countries_hashes
       load_all unless @@countries_hashes
       @@countries_hashes
     end
-    
-    def self.valid_attributes
-      countries_hashes.keys
+        
+    def self.method_missing(name, *args)
+      if match = /find_([^_]*)_by_([^_]*)/.match(name.to_s)
+        raise "1 argument expected, #{args.size} provided." unless args.size == 1
+        required = match[1]
+        request  = match[2].to_sym
+        if ATTRIBUTES.include?(request) && ATTRIBUTES.include?(required)
+          countries_hashes[request][args[0].to_s.downcase][required.to_sym] || nil rescue nil
+        else
+          raise "#{request} is not a valid attribute, valid attributes for find_*_by_* are: #{ATTRIBUTES.join(', ')}."
+        end
+        
+      elsif match = /find_by_(.*)/.match(name.to_s)
+        raise "1 argument expected, #{args.size} provided." unless args.size == 1
+        
+        request = match[1].to_sym
+        if ATTRIBUTES.include?(request)  
+          countries_hashes[request][args[0].to_s.upcase]
+        else
+          raise "#{request} is not a valid attribute, valid attributes for find_by_* are: #{ATTRIBUTES.join(', ')}."
+        end
+        
+      else
+        super
+      end
     end
     
     def self.load_all
@@ -117,13 +118,15 @@ module CountryTime
         country = Country.new(
           record[:name], record[:a2], record[:a3], record[:numeric]
         )
-        %w(name a2 a3 numeric).each do |field|
+        ATTRIBUTES.each do |field|
           @@countries_hashes[field][country.send(field)] = country
         end
       end
     end
     
-    attr_accessor :name, :a2, :a3, :numeric
+    ATTRIBUTES = [:name, :a2, :a3, :numeric]
+    
+    attr_accessor *ATTRIBUTES
     
     def initialize(name, a2, a3, numeric)
       @name, @a2, @a3, @numeric = name, a2, a3, numeric
